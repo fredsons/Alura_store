0aaf10b8f8e1b8a09f5e3d20811ec58b33bbf165
fsousaentre <85347233+fredsons@users.noreply.github.com>
2022-09-22 13:27:31 -0300

    atualiza

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,38 @@
-module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-        'no-console': 'off',
-    }
+// module.exports = {
+//     "env": {
+//         "es2021": true,
+//         "node": true
+//     },
+//     "extends": "eslint:recommended", "plugin:react/recommended"],
+//     "parserOptions": {
+//         "ecmaVersion": 12,
+//         "sourceType": "module"
+//     },
+//     "rules": {
+//         'no-console': 'off',
+//     },
 
-    'parserOptions': {
-        "ecmaVersion": 6,
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-        }
-    }
+//     'parserOptions': {
+//         "ecmaVersion": 2020,
+//         "ecmaFeatures": {
+//             "experimentalObjectRestSpread": true,
+//         }
+//     }
+
+// };
+
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaVersion: 8,
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+
+    rules: {
+        quotes: ["error", "double"],
+    },
 };
